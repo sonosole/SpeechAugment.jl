29af2efed725c13aec8596f55e850f8e4b9f7d1d
sole <taiyang_zhao@163.com>
2021-05-07 00:28:04 +0800

    style(test): adjust blanks
    adjust blanks in test\augment.jl

--- test/augment.jl
+++ test/augment.jl
@@ -12,11 +12,11 @@ batchsize = 8;
 data,fs = wavread("/home/xxx/ASpeechExample.wav");
 
 # 2. init all the augmentation functions you want
-echo = initAddEcho(fs, (0.05,0.4), (3.0,3.2,2.5,3.5,2.0,3.0));
+echo  = initAddEcho(fs, (0.05,0.4), (3.0,3.2,2.5,3.5,2.0,3.0));
 noise = initAddNoise("XXPathFullOfNoiseWAVs", 2, (5,15));
-clip = initClipWav((0.5,2.0));
-drop = initDropWav(fs, (0.09,0.15));
-far = initFarfieldWav(fs, (0.5,0.9));
+clip  = initClipWav((0.5,2.0));
+drop  = initDropWav(fs, (0.09,0.15));
+far   = initFarfieldWav(fs, (0.5,0.9));
 speed = initSpeedWav((0.8,1.2));
 
 # 3. make a function list
@@ -25,7 +25,7 @@ fnlist = [echo noise clip drop far speed];
 # 4. augment one wav into #batchSize audios
 wavlist = augmentWav(fnlist, data, batchsize)
 for i = 1:8
-    wavwrite(wavlist[i], "$i$i.wav",Fs=16000,nbits=32)
+    wavwrite(wavlist[i], "A$i.wav",Fs=16000,nbits=32)
 end
 
 
@@ -42,5 +42,5 @@ end
 # 2. augment #batchSize audios
 wavs = augmentWavs(fnlist, wavs)
 for i = 1:batchsize
-    wavwrite(wavs[i], "A$i.wav",Fs=16000,nbits=32)
+    wavwrite(wavs[i], "B$i.wav",Fs=16000,nbits=32)
 end
