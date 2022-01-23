0ec160ca2689c29b48fe6b6344128a614eafaec2
sole <taiyang_zhao@163.com>
2022-01-23 23:36:59 +0800

    refactor(speedwav): wider range of speed

--- src/speed.jl
+++ src/speed.jl
@@ -7,29 +7,35 @@ export speedWav
 change wav's speed. Speedup value ∈ [0.8,1.2] is normal and recommended.
 """
 function speedWav(wav::Array, k)
-    @assert 1.5>=k>=0.5
+    @assert k > 0
     N = veclen(wav);
     O = N>>1;
     X = fft(wav);
     D = floor(Int, k * O);
     L = D+1+D;
     Z = zeros(Complex{eltype(wav)}, L,1);
-    Z[1: 1:  O  ] = X[1: 1:  O  ];
-    Z[L:-1:L-O+1] = X[N:-1:N-O+1];
+    if k >= 1
+        Z[1: 1:  O  ] = X[1: 1:  O  ];
+        Z[L:-1:L-O+1] = X[N:-1:N-O+1];
+    else
+        Z[1: 1:  D  ] = X[1: 1:  D  ];
+        Z[L:-1:L-D+1] = X[N:-1:N-D+1];
+    end
     return real(ifft(Z))
 end
 
 
 """
-    initSpeedWav(speedSpan::NTuple{2,Number}) -> speedwav(wav::Array)
+    initSpeedWav(minspeed::AbstractFloat, maxspeed::AbstractFloat) -> speedwav(wav::Array)
 init speed perturbation effect function
-+ `speedSpan` e.g. (0.8, 1.2)
++ `minspeed` e.g. 0.8 means 0.8x
++ `maxspeed` e.g. 1.2 menas 1.2x
 """
-function initSpeedWav(speedSpan::NTuple{2,Number})
-    MinSpeed, MaxSpeed = speedSpan
-    @assert MinSpeed <= MaxSpeed
+function initSpeedWav(minspeed::AbstractFloat, maxspeed::AbstractFloat)
+    @assert minspeed <= maxspeed
     function speedwav(wav::Array)
-        return speedWav(wav, rand()*(MaxSpeed - MinSpeed) + MinSpeed)
+        k = rand()*(maxspeed - minspeed) + minspeed
+        return speedWav(wav, 1 / k)
     end
     return speedwav
 end
